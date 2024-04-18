bd943af5645e6a6fbd366e8e026b11226ad68fa9
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-04-18 14:39:22 -0400

    - Add reduce motion - Randomize gradient motion

--- scripts/gradient.js
+++ scripts/gradient.js
@@ -1,16 +1,28 @@
-let gradientAngle = 45;
-
 let gradientSpinElems = document.getElementsByClassName('gradient-spin');
-const gradientInterval = setInterval(function() {
-    updateGradientAngle();
-}, 50);
+let gradientAngles = Array.from({ length: gradientSpinElems.length }, () => Math.floor(Math.random() * 361) + 1);
+let gradientInterval;
+
+reduceMotion();
 
 function updateGradientAngle() {
-    gradientAngle += 1
-    if (gradientAngle > 360) {
-        gradientAngle = 0;
+    for (let i = 0; i < gradientSpinElems.length; i++) {
+        gradientAngles[i] += Math.floor(Math.random() * 5) - 5;
+        if (gradientAngles[i] > 360) {
+            gradientAngles[i] = 0;
+        }
+        gradientSpinElems[i].style.background = `linear-gradient(${gradientAngles[i]}deg, #1a9fff, #946beb)`;
     }
-    for (let i=0; i<gradientSpinElems.length; i++) {
-        gradientSpinElems[i].style.background = `linear-gradient(${gradientAngle}deg, #1a9fff, #946beb)`;
+}
+
+function reduceMotion() {
+    let reduceMotionState = document.getElementById('reduce-motion-switch').checked;
+    console.log(reduceMotionState);
+    if (reduceMotionState) {
+        clearInterval(gradientInterval);
+        console.log('stopped');
+    } else {
+        gradientInterval = setInterval(function () {
+            updateGradientAngle();
+        }, 100);
     }
 }
