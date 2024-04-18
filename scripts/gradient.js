ef5b095396f86bbb958831d1aa88f3000c77d75e
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-04-18 12:22:51 -0400

    Add features page

--- scripts/gradient.js
+++ scripts/gradient.js
@@ -1,17 +1,16 @@
 let gradientAngle = 45;
 
-let jumbotronElems = document.getElementsByClassName('jumbotron');
+let gradientSpinElems = document.getElementsByClassName('gradient-spin');
 const gradientInterval = setInterval(function() {
     updateGradientAngle();
 }, 50);
-console.log(jumbotronElems.length)
 
 function updateGradientAngle() {
     gradientAngle += 1
     if (gradientAngle > 360) {
         gradientAngle = 0;
     }
-    for (let i=0; i<jumbotronElems.length; i++) {
-        jumbotronElems[i].style.background = `linear-gradient(${gradientAngle}deg, #1a9fff, #946beb)`;
+    for (let i=0; i<gradientSpinElems.length; i++) {
+        gradientSpinElems[i].style.background = `linear-gradient(${gradientAngle}deg, #1a9fff, #946beb)`;
     }
 }
