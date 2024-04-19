812831a390b74d554859612fee1d19f3dcf120d1
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-04-19 09:39:02 -0400

    Adjust gradient angle rotation

--- scripts/gradient.js
+++ scripts/gradient.js
@@ -7,19 +7,14 @@ reduceMotion();
 function updateGradientAngle() {
     for (let i = 0; i < gradientSpinElems.length; i++) {
         gradientAngles[i] += Math.floor(Math.random() * 5) - 5;
-        if (gradientAngles[i] > 360) {
-            gradientAngles[i] = 0;
-        }
-        gradientSpinElems[i].style.background = `linear-gradient(${gradientAngles[i]}deg, #1a9fff, #946beb)`;
+        gradientSpinElems[i].style.background = `linear-gradient(${gradientAngles[i]}deg, #1a9fff, 15%, #946beb)`;
     }
 }
 
 function reduceMotion() {
     let reduceMotionState = document.getElementById('reduce-motion-switch').checked;
-    console.log(reduceMotionState);
     if (reduceMotionState) {
         clearInterval(gradientInterval);
-        console.log('stopped');
     } else {
         gradientInterval = setInterval(function () {
             updateGradientAngle();
