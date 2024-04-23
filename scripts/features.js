2432ca864db57feb17281847a758340ed94b95a2
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-04-23 13:14:44 -0400

    Update style on feature-title-list

--- scripts/features.js
+++ scripts/features.js
@@ -1,20 +1,31 @@
 featureTitles = document.getElementsByClassName('feature-list-item');
 featureDetails = document.getElementsByClassName('feature-detail-container');
-let currentFeature = 0;
+// let currentFeature = 0;
 
 for (let i = 0; i < featureTitles.length; i++) {
     featureTitles[i].addEventListener('click', function () {
         updateActiveFeature(i);
     }, false)
+    if (featureTitles[i].id != 'active-feature-item') {
+        featureTitles[i].className = 'row feature-list-item rounded-3';
+    }
 }
 
 function updateActiveFeature(i) {
-    currentFeature = i;
+    // Reset all
     for (let j = 0; j < featureTitles.length; j++) {
         featureTitles[j].removeAttribute('id');
         featureDetails[j].removeAttribute('id');
+        featureTitles[j].className = 'row feature-list-item rounded-3';
+        featureTitles[j].style = '';
     }
 
+    // currentFeature = i;
+
+    // Update the active feature
     featureTitles[i].id = 'active-feature-item';
     featureDetails[i].id = 'active-feature-detail'
+
+    // Handle styles
+    featureTitles[i].className += ' gradient-spin my-gradient';
 }
