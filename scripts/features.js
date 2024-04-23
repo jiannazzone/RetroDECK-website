8c57752adacd55ae6b0aae89243c6bc19aa85215
Adam Iannazzone <adam.iannazzone@gmail.com>
2024-04-23 18:34:02 -0400

    Revert "Add exterior divs to reduce number of gradient calculations"
    This reverts commit c02eb378146e816592b6db1b29e3a610578be2e5.

--- scripts/features.js
+++ scripts/features.js
@@ -1,5 +1,6 @@
 featureTitles = document.getElementsByClassName('feature-list-item');
 featureDetails = document.getElementsByClassName('feature-detail-container');
+// let currentFeature = 0;
 
 for (let i = 0; i < featureTitles.length; i++) {
     featureTitles[i].addEventListener('click', function () {
@@ -15,9 +16,16 @@ function updateActiveFeature(i) {
     for (let j = 0; j < featureTitles.length; j++) {
         featureTitles[j].removeAttribute('id');
         featureDetails[j].removeAttribute('id');
+        featureTitles[j].className = 'row feature-list-item rounded-3';
+        featureTitles[j].style = '';
     }
+
+    // currentFeature = i;
 
     // Update the active feature
     featureTitles[i].id = 'active-feature-item';
     featureDetails[i].id = 'active-feature-detail'
+
+    // Handle styles
+    featureTitles[i].className += ' gradient-spin my-gradient';
 }
