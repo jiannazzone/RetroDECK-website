dd8aeca6f797acb186987c9b23e925bc38e098f7
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-09-23 19:57:27 -0400

    Add preliminary features.js - Add feature cards - Add eventListener to cards - Add relevant styles

--- scripts/features.js
+++ scripts/features.js
@@ -1,51 +1,26 @@
-const featureTitles = document.getElementsByClassName('feature-list-item');
-const featureDetails = document.getElementsByClassName('feature-detail-container');
-const featureList = document.getElementById('feature-list');
-let currentFeature = 0;
+const featureCardElems = document.getElementsByClassName('feature-card');
+const featureCardContainerElem = document.getElementById('feature-card-container');
+let currentFeatureCard = 1
 
-for (let i = 0; i < featureTitles.length; i++) {
-    featureTitles[i].addEventListener('click', function () {
-        if (i != currentFeature) {
-            updateActiveFeature(i);
+for (let i = 0; i < featureCardElems.length; i++) {
+    featureCardElems[i].addEventListener('click', function() {
+        if (i != currentFeatureCard) {
+            updateActiveFeatureCard(i);
         }
     }, false)
-
-    // We need to remove the gradient so that linearBackground is hidden
-    if (featureTitles[i].id != 'active-feature-item') {
-        featureTitles[i].className = 'col feature-list-item rounded-3 m-0';
-    }
 }
 
-function updateActiveFeature(i) {
-    const lastCurrentFeature = currentFeature;
-    currentFeature = i;
-    // console.log(`${lastCurrentFeature} -> ${currentFeature}`);
+function updateActiveFeatureCard(i) {
+    const lastCurrentFeature = currentFeatureCard;
+    currentFeatureCard = i;
+    console.log(i)
 
-    // New current feature
-    featureTitles[currentFeature].className += ' gradient-spin my-gradient';
-    featureTitles[currentFeature].id = 'active-feature-item';
-    featureDetails[currentFeature].id = 'active-feature-detail'
-
-    // Reset previous current feature
-    featureTitles[lastCurrentFeature].removeAttribute('id');
-    featureTitles[lastCurrentFeature].className = 'col feature-list-item rounded-3 m-0';
-    featureTitles[lastCurrentFeature].style = '';
-    featureDetails[lastCurrentFeature].removeAttribute('id');
-
-    // Update scroll position
-    const offsetPadding = 20;
-    if (lastCurrentFeature < currentFeature) {
-        featureList.scrollLeft = featureTitles[currentFeature].offsetLeft - offsetPadding;
+    featureCardElems[i].id = 'active-feature-card';
+    featureCardElems[lastCurrentFeature].id = "";
+    if (lastCurrentFeature < i) {
+        // featureCardContainerElem.scrollLeft = featureCardElems[i].offsetLeft - featureCardElems[i].offsetWidth/2
+        featureCardContainerElem.scrollIntoView
     } else {
-        featureList.scrollLeft = featureTitles[currentFeature].offsetLeft - featureTitles[currentFeature].offsetWidth/2 - offsetPadding;
+        featureCardContainerElem.scrollLeft = featureCardElems[i].offsetLeft - featureCardElems[i].offsetWidth
     }
-    
 }
-
-const widthOutput = document.querySelector("#width");
-window.addEventListener('resize', function() {
-    const featureListElem = document.getElementById('feature-list');
-    if (widthOutput < 768) {
-        featureListElem.classList += "text-nowrap";
-    }
-})
