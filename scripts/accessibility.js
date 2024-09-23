645b855120e6e593caf802384ca208f3438951f1
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-09-23 19:56:49 -0400

    Update style of accessibility.js

--- scripts/accessibility.js
+++ scripts/accessibility.js
@@ -1,10 +1,10 @@
 function pixelFontToggle() {
-    const pixelFontDefault = "\"Upheaval TT\", monospace;"
+    const pixelFontDefault = '\"Upheaval TT\", monospace;'
 
-    const pixelFontSwitchElem = document.getElementById("pixel-font-switch");
+    const pixelFontSwitchElem = document.getElementById('pixel-font-switch');
     if (pixelFontSwitchElem.checked) {
-        document.documentElement.style.cssText = "--pixel-font: \"Upheaval TT\", monospace";
+        document.documentElement.style.cssText = '--pixel-font: \"Upheaval TT\", monospace';
     } else {
-        document.documentElement.style.cssText = "--pixel-font: \"Source Sans 3\", sans-serif";
+        document.documentElement.style.cssText = '--pixel-font: \"Source Sans 3\", sans-serif';
     }
 }
