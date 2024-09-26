155a3ae8092ec0e72bb9eb412c410d6f122c692f
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-09-26 13:18:18 -0400

    Add keyboard control to carousel

--- scripts/features.js
+++ scripts/features.js
@@ -1,38 +1,49 @@
-const featureContainerElems = document.getElementsByClassName('feature-container');
-let currentFeature = 1
+// const featureContainerElems = document.getElementsByClassName('feature-container');
+// let currentFeature = 1
 
-for (let i = 0; i < featureContainerElems.length; i++) {
-    featureContainerElems[i].addEventListener('click', function() {
-        if (i != currentFeature) {
-            updateActiveFeature(i);
-        }
-    })
-}
+// for (let i = 0; i < featureContainerElems.length; i++) {
+//     featureContainerElems[i].addEventListener('click', function() {
+//         if (i != currentFeature) {
+//             updateActiveFeature(i);
+//         }
+//     })
+// }
 
-document.addEventListener('keyup', keyControl);
+// document.addEventListener('keyup', keyControl);
 
-function keyControl(e) {
+// function keyControl(e) {
+//     if (e.code == 'ArrowRight') {
+//         if (currentFeature < featureContainerElems.length-2) {
+//             updateActiveFeature(currentFeature + 1);
+//         } else {
+//             updateActiveFeature(1);
+//         }
+//     }
+//     if (e.code == 'ArrowLeft') {
+//         if (currentFeature > 1) {
+//             updateActiveFeature(currentFeature - 1);
+//         } else {
+//             updateActiveFeature(featureContainerElems.length-2);
+//         }
+//     }
+// }
+
+// function updateActiveFeature(i) {
+//     const lastCurrentFeature = currentFeature;
+//     currentFeature = i;
+
+//     featureContainerElems[i].id = 'active-feature';
+//     featureContainerElems[i].scrollIntoView({block: 'start', inline: 'center'});
+//     featureContainerElems[lastCurrentFeature].id = ''
+// }
+
+
+document.addEventListener('keyup', function(e) {
     if (e.code == 'ArrowRight') {
-        if (currentFeature < featureContainerElems.length-2) {
-            updateActiveFeature(currentFeature + 1);
-        } else {
-            updateActiveFeature(1);
-        }
+        document.getElementById('carousel-next').click();
+        document.getElementById('features').scrollIntoView({block: 'center'});
+    } else if (e.code == 'ArrowLeft') {
+        document.getElementById('carousel-prev').click();
+        document.getElementById('features').scrollIntoView({block: 'center'});
     }
-    if (e.code == 'ArrowLeft') {
-        if (currentFeature > 1) {
-            updateActiveFeature(currentFeature - 1);
-        } else {
-            updateActiveFeature(featureContainerElems.length-2);
-        }
-    }
-}
-
-function updateActiveFeature(i) {
-    const lastCurrentFeature = currentFeature;
-    currentFeature = i;
-
-    featureContainerElems[i].id = 'active-feature';
-    featureContainerElems[i].scrollIntoView({block: 'start', inline: 'center'});
-    featureContainerElems[lastCurrentFeature].id = ''
-}
+});
